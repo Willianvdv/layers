9ddce0c29358eb1fcc2cd10703c796d0c1b4e1df
Willian van der Velde <willian@pythonheads.nl>
2014-07-03 11:50:27 +0200

    Basic event test

--- layer_core/test/models/event_test.rb
+++ layer_core/test/models/event_test.rb
@@ -2,4 +2,11 @@
 require 'test_helper'
 
 class EventTest < ActiveSupport::TestCase
+  setup do
+    @event = FactoryGirl.build :event
+  end
+
+  test 'event is valid' do
+    assert @event.valid?
+  end
 end
--- layer_core/test/test_helper.rb
+++ layer_core/test/test_helper.rb
@@ -4,6 +4,10 @@ ENV["RAILS_ENV"] = "test"
 require File.expand_path("../dummy/config/environment.rb",  __FILE__)
 require "rails/test_help"
 
+require 'layer_core/factories/user'
+require 'layer_core/factories/item'
+require 'layer_core/factories/event'
+
 Rails.backtrace_cleaner.remove_silencers!
 
 # Load support files
@@ -12,4 +16,9 @@ Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
 # Load fixtures from the engine
 if ActiveSupport::TestCase.method_defined?(:fixture_path=)
   ActiveSupport::TestCase.fixture_path = File.expand_path("../fixtures", __FILE__)
+end
+
+class ActiveSupport::TestCase
+  ActiveRecord::Migration.check_pending!
+  include FactoryGirl::Syntax::Methods
 end
